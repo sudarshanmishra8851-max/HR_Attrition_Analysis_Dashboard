07eb8e393097595b6e0ddf2c6eea1e0c820c134e
sudarshanmishra8851-max <sudarshanmishra8851@gmail.com>
2025-12-25 12:39:11 +0530

    Add files via upload

---
+++ HR_ATTRITION.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE ATTRITION 
+
+USE ATTRITION
+
+CREATE TABLE Employees (
+Employee_ID INT,
+Age INT,
+Attrition CHAR(3),
+Business_Travel VARCHAR(20),
+Department VARCHAR(30),
+Distance_From_Home INT,
+Education VARCHAR(15),
+Environment_Satisfaction INT,
+Gender VARCHAR(10),
+Salary Float,
+Job_Involvement INT,
+job_level INT,
+Job_Role VARCHAR(20),
+Job_Satisfaction INT,
+Marital_Status VARCHAR(30),
+Number_of_Companies_Worked_previously INT,
+Overtime CHAR(5),
+Salary_Hike_in_percent INT,
+Total_working_years_experience INT,
+Work_life_balance INT,
+No_of_years_worked_at_current_company INT,
+No_of_years_in_current_role INT,
+Years_since_last_promotion INT
+)
+
+SELECT * FROM Employees
+ 
+BULK INSERT Employees
+FROM 'C:\Users\Roshan Mishra\Desktop\project\PROJECT HR ATTRIBITION\archive\HR_Attrition_dataset.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n'
+		)
